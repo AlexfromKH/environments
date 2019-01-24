392fcf1a976780f5360570d6f1721fea54ccb337
AlexfromKH <40006674+AlexfromKH@users.noreply.github.com>
2019-01-24 21:54:12 +0100

    add profile::mattermost

--- site/profile/manifests/mattermost.pp
+++ site/profile/manifests/mattermost.pp
@@ -1,8 +1,10 @@
-class { 'mattermost':
-  override_options => {
-    'SqlSettings' => {
-      'DriverName' => 'postgres',
-      'DataSource' => "postgres://mattermost:mattermost@127.0.0.1:5432/mattermost?sslmode=disable&connect_timeout=10",
+class profile::mattermost {
+  class { 'mattermost':
+    override_options => {
+      'SqlSettings' => {
+        'DriverName' => 'postgres',
+        'DataSource' => "postgres://mattermost:mattermost@127.0.0.1:5432/mattermost?sslmode=disable&connect_timeout=10",
+      },
     },
-  },
+  }
 }
