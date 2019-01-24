7c6945c0dd91168f535dcdeebd21993702cf6fb3
AlexfromKH <40006674+AlexfromKH@users.noreply.github.com>
2019-01-24 21:51:31 +0100

    add profile::nginx

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -1,13 +1,15 @@
-class { 'nginx': }
-nginx::resource::upstream { 'mattermost':
-  members => [ 'localhost:8065' ],
-}
-nginx::resource::server { 'mattermost':
-  server_name         => [ 'myserver.mydomain' ],
-  proxy               => 'http://mattermost',
-  location_cfg_append => {
-    'proxy_http_version'          => '1.1',
-    'proxy_set_header Upgrade'    => '$http_upgrade',
-    'proxy_set_header Connection' => '"upgrade"',
-  },
+class profile::nginx {
+  class { 'nginx': }
+  nginx::resource::upstream { 'mattermost':
+    members => [ 'localhost:8065' ],
+  }
+  nginx::resource::server { 'mattermost':
+    server_name         => [ 'myserver.mydomain' ],
+    proxy               => 'http://mattermost',
+    location_cfg_append => {
+      'proxy_http_version'          => '1.1',
+      'proxy_set_header Upgrade'    => '$http_upgrade',
+      'proxy_set_header Connection' => '"upgrade"',
+    },
+  }
 }
