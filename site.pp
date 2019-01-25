4c88bc24fb2f71bd2d0583a05cc6a1a240fc7810
root <dekormag@gmail.com>
2019-01-25 13:38:09 +0000

    add nginx

--- site.pp
+++ site.pp
@@ -1,9 +1,9 @@
 # hiera_include('classes')
 #include puppet_env
 #include puppet_webserver
-node default {
-  include role::lamp
-}
+# node default {
+  # include role::lamp
+# }
 node "centos-agent" {
   include role::forge_mattermost
 }
