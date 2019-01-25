dfdbc376d5d5e0715afcc7ebdfd74c50b931f155
root <dekormag@gmail.com>
2019-01-25 16:03:33 +0000

    add nginx

--- site/profile/manifests/mattermost.pp
+++ site/profile/manifests/mattermost.pp
@@ -1,11 +1,10 @@
 class profile::mattermost {
   class { 'mattermost':
     override_options => {
-     'ServiceSettings' => {
+      'ServiceSettings' => {
         'ListenAddress' => ':80',
-    },
-    }
-      'SqlSettings'  => {
+      },
+      'SqlSettings'     => {
         'DriverName' => 'postgres',
         'DataSource' => 'postgres://mattermost:mattermost@127.0.0.1:5432/mattermost?sslmode=disable&connect_timeout=10',
       },
