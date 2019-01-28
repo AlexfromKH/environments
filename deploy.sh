4dae681a608ab68ba5c587014966f794cbea2fc4
root <dekormag@gmail.com>
2019-01-28 16:01:12 +0000

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,22 @@ case "$2" in
     branch="staging"
     ;;
   *)
-     echo "Usage: deploy {comment} {-p (production) | -t (test) | -s (staging | -m (master)}"
-     exit 1
+    2>/dev/null
+    echo "Usage: deploy {comment} {-p (production) | -t (test) | -s (staging | -m (master)}"
+    exit 1
 esac  
 git push origin "$branch"
-echo *******************************
-echo "files commited with $1 pushed to $branch"
-echo *******************************
+# deploy environment with r10k
+if [ $3 -z ] && [ $3 == "-r" ]; then
+  r10k deploy environment -p
+  echo "*******************************"
+  echo "files commited with $1 pushed to $branch and deployed with r10k to puppet environment"
+  echo "*******************************"
+elif [ $3 !=="-r" ]; then
+  2>/dev/null
+  echo "do deploy environment with r10k enter -r"
+esle
+  echo "*******************************"
+  echo "files commited with $1 pushed to $branch"
+  echo "*******************************"
+fi
