a2023b1568ef6a49006611bb2fe3e7ac2448f7f2
root <dekormag@gmail.com>
2019-01-28 16:08:27 +0000

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,12 +21,12 @@ case "$2" in
 esac  
 git push origin "$branch"
 # deploy environment with r10k
-if [ $3 -z ] && [ $3 == "-r" ]; then
+if [ $3 -n ] && [ $3 -eq "-r" ]; then
   r10k deploy environment -p
   echo "*******************************"
   echo "files commited with $1 pushed to $branch and deployed with r10k to puppet environment"
   echo "*******************************"
-elif [ $3 !=="-r" ]; then
+elif [ $3 -ne "-r" ]; then
   2>/dev/null
   echo "do deploy environment with r10k enter -r"
 esle
