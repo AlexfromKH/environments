c807a14a9911ee337cafb63adfa2f9bdfa48ada3
root <dekormag@gmail.com>
2019-01-28 16:36:46 +0000

    ready deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,13 +26,13 @@ git push origin "$branch"
 if [ -n $deploy ] && [ $deploy == "-r" ]; then
   r10k deploy environment -p
   echo "*******************************"
-  echo "files commited with $1 pushed to $branch and deployed with r10k to puppet environment"
+  echo "files commited with \"$1\", pushed to \"$branch\" and deployed with r10k to puppet environment"
   echo "*******************************"
 elif [ $deploy !== "-r" ]; then
   echo "do deploy environment with r10k enter -r"
   exit 1
 elif [ -z $deploy ]; then
   echo "*******************************"
-  echo "files commited with $1 pushed to $branch"
+  echo "files commited with \"$1\" and pushed to \"$branch\""
   echo "*******************************"
 fi
